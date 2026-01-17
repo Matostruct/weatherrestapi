b40e8c36fe92f32e7fe1dd4ec73abcbfe9013e64
Patrick Riedler <patrick.riedler@edu.fh-joanneum.at>
2026-01-17 11:03:20 +0200

    Initial Spring Boot setup

---
+++ src/test/java/at/fh/weather/weatherrestapi/WeatherrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fh.weather.weatherrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
